782944539fdcce464c5af3d290c2216397d36873
SagarRajoriya <138926328+SagarRajoriya@users.noreply.github.com>
2025-06-14 00:24:21 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
-import aboutImage from "./public/assets/Dr.ShraddhaSharmaWorking.jpg";
+import aboutImage from "../images/Dr.ShraddhaSharmaWorking.jpg";
 
 const About = () => {
   return (
